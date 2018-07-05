a1c0c027ab935440e1530bfadf6aaf10d1831ded
SeverinLangberg <Langberg91@gmail.com>
2018-07-05 22:35:39 +0200

    Bump version: 0.2.3 → 0.2.4

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ LICENSE = "MIT"
 URL = 'https://github.com/GSEL9/pydavis'
 AUTHOR = 'Severin Langberg'
 EMAIL = 'langberg91@gmail.com'
-VERSION = '0.2.3'
+VERSION = '0.2.4'
 
 TESTS_REQUIRE = ['pytest', 'pytest_mock', 'nose']
 
